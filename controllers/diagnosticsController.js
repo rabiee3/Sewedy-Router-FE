cf3ec9ae3e2658685f4b34fb533c82fab999ffd3
Rabie Mohamed <rabiee3.mohamed@gmail.com>
2025-10-07 08:14:43 +0300

    updating traceroute functionality

--- controllers/diagnosticsController.js
+++ controllers/diagnosticsController.js
@@ -324,173 +324,160 @@ myapp.controller("diagnosticsController", function(
   };
 
   $scope.startTracerouteTest = function(object) {
+    $scope.erroronvalidation = false;
+    $scope.tracerouteArray = [];
+    $scope.errorMessage = "";
     $scope.loadingtrace = false;
     $scope.loadingtrace1 = true;
-    if (
-      $scope["TraceRouteHost"] !== "" &&
-      $scope["TraceRouteHost"] !== undefined
-    ) {
-      //test ipv4
 
-      $scope.validipv4 = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/.test(
-        $scope["TraceRouteHost"]
-      );
+    const host = $scope.TraceRouteHost?.trim();
 
-      //test ipv6
-
-      $scope.validipv6 = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){4}(((:[0-9A-Fa-f]{1,4}){1,3})|((:[0-9A-Fa-f]{1,4})?:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){3}(((:[0-9A-Fa-f]{1,4}){1,4})|((:[0-9A-Fa-f]{1,4}){0,2}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){2}(((:[0-9A-Fa-f]{1,4}){1,5})|((:[0-9A-Fa-f]{1,4}){0,3}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(([0-9A-Fa-f]{1,4}:){1}(((:[0-9A-Fa-f]{1,4}){1,6})|((:[0-9A-Fa-f]{1,4}){0,4}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(:(((:[0-9A-Fa-f]{1,4}){1,7})|((:[0-9A-Fa-f]{1,4}){0,5}:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(%.+)?\s*$/.test(
-        $scope["TraceRouteHost"]
-      );
-
-      $scope.validurl = /^((https?|ftp):\/\/)?([a-z]+[.])?[a-z0-9-]+([.][a-z]{1,4}){1,2}(\/.*[?].*)?$/i.test(
-        $scope["TraceRouteHost"]
-      );
-      console.info("scopy", $scope["TraceRouteHost"]);
-      console.info("url test", $scope.validurl);
-      console.info("ipv4 test", $scope.validipv4);
-      console.info("ipv6 test", $scope.validipv6);
-
-      if ($scope.validurl || $scope.validipv4 || $scope.validipv6) {
-        console.info("scope success", $scope["TraceRouteHost"]);
-        urlstatus = false;
-        var post = "";
-        var url = URL + "cgi_set?";
-        var formobjects = object.split("?");
-        angular.forEach(formobjects, function(object) {
-          var objectlevelurlstatus = false;
-          var postobject = "Object=" + object + "&Operation=Modify";
-          //                angular.forEach($scope[object.replace(/\./g, "")], function (value, key) {
-          //                if (traceroutetestchangedFields.indexOf(object.replace(/\./g, "") + key) > -1) {
-          objectlevelurlstatus = true;
-          urlstatus = true;
-          postobject += "&Host" + "=" + $scope["TraceRouteHost"];
-          //                }
-          //                });
-          if (objectlevelurlstatus) post += postobject + ",";
-        });
-        post = post.replace(/(^[,\s]+)|([,\s]+$)/g, "");
-        //alert(post);
-        if (urlstatus) {
-          var setData = function() {
-            $http
-              .post(url, post)
-              .success(function(data, status, headers, config) {
-                if (status === 200) {
-                  setTimeout(function() {
-                    getFormData(
-                      "cgi_get?Object=Device.IP.Diagnostics.TraceRoute"
-                    );
-                  }, 100);
-                } else if (500 <= status && status < 600) {
-                  $scope["traceroute" + "popup"] = true;
-                  $scope["traceroute" + "popupval"] =
-                    data.Objects[0].Param[0].ParamValue;
-                } else if (400 <= status && status < 500) {
-                  angular.forEach(data.Objects, function(object) {
-                    var respobject = object.ObjName.replace(/\./g, "").replace(
-                      /\*/g,
-                      ""
-                    );
-                    angular.forEach(object.Param, function(param) {
-                      $scope[
-                        respobject + "_" + param.ParamName + "responsestatus"
-                      ] = true;
-                      $scope[respobject + "_" + param.ParamName + "val"] =
-                        param.ParamValue;
-                    });
-                  });
-                } else if (status === TOKEN_MISMATCH_CODE) {
-                  setData();
-                }
-              })
-              .error(function(data, status, headers, config) {});
-          };
-          setData();
-        } else {
-          alert("Parameter is Empty");
-        }
-      } else {
-        $scope.loadingtrace = false;
-        $scope.loadingtrace1 = false;
-
-        $scope.erroronvalidation = true;
-      }
-    } else {
+    if (!host) {
       alert("Value is Empty");
+      $scope.loadingtrace1 = false;
+      return;
     }
+
+
+    const ipv4Regex = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
+    const ipv6Regex = /^\s*((([0-9A-Fa-f]{1,4}:){7}([0-9A-Fa-f]{1,4}|:))|(([0-9A-Fa-f]{1,4}:){6}(:[0-9A-Fa-f]{1,4}|((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(([0-9A-Fa-f]{1,4}:){5}(((:[0-9A-Fa-f]{1,4}){1,2})|:((25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:)))(%.+)?\s*$/;
+    const urlRegex = /^((https?|ftp):\/\/)?([a-z0-9-]+[.])+[a-z]{2,}(:\d+)?(\/.*)?$/i;
+    const valid =
+      ipv4Regex.test(host) || ipv6Regex.test(host) || urlRegex.test(host);
+    if (!valid) {
+      $scope.loadingtrace = false;
+      $scope.loadingtrace1 = false;
+      $scope.erroronvalidation = true;
+      return;
+    }
+    const url = URL + "cgi_set?";
+    const objects = object.split("?");
+    let post = "";
+    angular.forEach(objects, function(obj) {
+      const postobject = "Object=" + obj + "&Operation=Modify&Host=" + host;
+      post += postobject + ",";
+    });
+    post = post.replace(/(^[,\s]+)|([,\s]+$)/g, "");
+    const setData = function() {
+      $http.post(url, post).then(
+        function(response) {
+          const status = response.status;
+          const data = response.data;
+
+          if (status === 200) {
+            setTimeout(() => {
+              getFormData("cgi_get?Object=Device.IP.Diagnostics.TraceRoute")
+                .then(displayTracerouteResult)
+                .catch(() => {
+                  $scope.errorMessage = "Failed to fetch traceroute results.";
+                });
+            }, 3000);
+          } else if (status === TOKEN_MISMATCH_CODE) {
+            setData();
+          } else {
+            $scope.errorMessage = "Error: " + status;
+          }
+        },
+        function(error) {
+          $scope.errorMessage = "HTTP Error: " + error.status;
+          $scope.loadingtrace = false;
+          $scope.loadingtrace1 = false;
+        }
+      );
+    };
+
+    // Show progress bar and send
+    $scope.loadingtrace1 = true;
+    setData();
   };
+
+  // This helper processes your cgi_get response
+  function displayTracerouteResult(data) {
+    $scope.loadingtrace1 = false;
+    $scope.loadingtrace = true;
+
+    if (!data || !data.Objects) {
+      $scope.errorMessage = "No response from device.";
+      return;
+    }
+
+    const traceObj = data.Objects.find(
+      (o) => o.ObjName === "Device.IP.Diagnostics.TraceRoute"
+    );
+    const stateParam = traceObj?.Param?.find(
+      (p) => p.ParamName === "DiagnosticsState"
+    );
+    const state = stateParam ? stateParam.ParamValue : "Unknown";
+
+    const hops = data.Objects.filter((o) =>
+      o.ObjName.startsWith("Device.IP.Diagnostics.TraceRoute.RouteHops.")
+    );
+    $scope.tracerouteArray = hops.map((hop) => {
+      const getParam = (n) =>
+        hop.Param.find((p) => p.ParamName === n)?.ParamValue || "-";
+      return {
+        Host: getParam("Host"),
+        HostAddress: getParam("HostAddress"),
+        RTTimes: getParam("RTTimes"),
+      };
+    });
+
+    if (state !== "Complete") {
+      $scope.errorMessage =
+        "TraceRoute ended with: " +
+        state.replace("Error_", "").replace(/_/g, " ");
+    }
+
+    $scope.$applyAsync();
+  }
 
   getFormData = function(reqParams) {
-    $http
-      .get(URL + reqParams)
-      .success(function(data, status, headers, config) {
-        if (status === 200) {
-          objects = data.Objects;
-          $scope.diagnoseArray = [];
-          for (var obj = 0; obj < objects.length; obj++) {
-            if (objects[obj].ObjName === "Device.IP.Diagnostics.TraceRoute") {
-              var objectParamValues = objects[obj].Param;
-              var diagnoseobject = {};
-              for (var i = 0; i < objectParamValues.length; i++) {
-                if (objectParamValues[i].ParamName === "DiagnosticsState") {
-                  var param_name = objectParamValues[i].ParamName;
-                  var param_value = objectParamValues[i].ParamValue;
-                  diagnoseobject[param_name] = param_value;
-                } else if (
-                  objectParamValues[i].ParamName === "RouteHopsNumberOfEntries"
-                ) {
-                  var param_name = objectParamValues[i].ParamName;
-                  var param_value = objectParamValues[i].ParamValue;
-                  diagnoseobject[param_name] = param_value;
-                }
-              }
+    return $http.get(URL + reqParams).then(function(response) {
+      var data = response.data;
+      var status = response.status;
+
+      if (status === 200) {
+        var objects = data.Objects;
+        $scope.diagnoseArray = [];
+        for (var obj = 0; obj < objects.length; obj++) {
+          if (objects[obj].ObjName === "Device.IP.Diagnostics.TraceRoute") {
+            var objectParamValues = objects[obj].Param;
+            var diagnoseobject = {};
+            for (var i = 0; i < objectParamValues.length; i++) {
+              var paramName = objectParamValues[i].ParamName;
+              var paramValue = objectParamValues[i].ParamValue;
               if (
-                diagnoseobject.DiagnosticsState ===
-                "Error_CannotResolveHostName"
+                paramName === "DiagnosticsState" ||
+                paramName === "RouteHopsNumberOfEntries"
               ) {
-                $scope.loadingtrace = true;
-                $scope["error_responsestatus"] = true;
-                console.log($scope["error_responsestatus"]);
-                $scope["err_val"] = "Cannot resolve host name.";
-                $scope.loadingtrace1 = false;
-              } else {
-                getTraceRoute(
-                  "cgi_get?Object=Device.IP.Diagnostics.TraceRoute.RouteHops",
-                  diagnoseobject
-                );
+                diagnoseobject[paramName] = paramValue;
               }
             }
-          }
-        } else if (500 <= status && status < 600) {
-          $scope["routehopentries" + "popup"] = true;
-          $scope["routehopentries" + "popupval"] =
-            data.Objects[0].Param[0].ParamValue;
-        } else if (400 <= status && status < 500) {
-          if (data.Objects.length < 2 && data.Objects[0].Param.length < 2) {
-            $scope["routehopentries" + "popup"] = true;
-            $scope["routehopentries" + "popupval"] =
-              data.Objects[0].Param[0].ParamValue;
-          } else {
-            angular.forEach(data.Objects, function(object) {
-              var respobject = object.ObjName.replace(/\./g, "").replace(
-                /\*/g,
-                ""
+            if (
+              diagnoseobject.DiagnosticsState === "Error_CannotResolveHostName"
+            ) {
+              $scope.loadingtrace = true;
+              $scope["error_responsestatus"] = true;
+              $scope["err_val"] = "Cannot resolve host name.";
+              $scope.loadingtrace1 = false;
+            } else {
+              getTraceRoute(
+                "cgi_get?Object=Device.IP.Diagnostics.TraceRoute.RouteHops",
+                diagnoseobject
               );
-              angular.forEach(object.Param, function(param) {
-                $scope[
-                  respobject + "_" + param.ParamName + "responsestatus"
-                ] = true;
-                $scope[respobject + "_" + param.ParamName + "val"] =
-                  param.ParamValue;
-              });
-            });
+            }
           }
-        } else if (status === TOKEN_MISMATCH_CODE) {
-          getFormData(reqParams);
         }
-      })
-      .error(function(data, status, headers, config) {});
+        return data;
+      }
+      if (status === TOKEN_MISMATCH_CODE) {
+        return getFormData(reqParams);
+      } else {
+        return $q.reject({ status, data });
+      }
+    });
   };
+
   getTraceRoute = function(reqParams, diagnoseobject) {
     $scope.loadingtrace = true;
     /*  $scope.loadingtrace1 = true;*/
