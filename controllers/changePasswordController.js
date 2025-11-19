2ca676b7794439cbc75d7d1dfd8baf5a203f1e9b
Rabie Mohamed <rabiee3.mohamed@gmail.com>
2025-11-19 17:07:12 +0200

    update

--- controllers/changePasswordController.js
+++ controllers/changePasswordController.js
@@ -14,31 +14,55 @@ myapp.controller("changePasswordController", function(
   pageloadiconstatus = true;
   var previoousmessages = [];
   $scope.formsubmitted = false;
+  $scope.passwords = {
+    userpassword: "",
+    confirmpassword: "",
+  };
 
   $scope.checkPasswordsAreSame = function() {
-    if ($scope.userpassword !== undefined) {
-      if ($scope.userpassword === $scope.confirmpassword) {
-        $scope.passwordmismatch = false;
-        return true;
-      } else {
-        $scope.passwordmismatch = true;
-        return false;
-      }
+    if ($scope.changePassword.userpassword.$error.pattern) {
+      $scope.passwordmismatch = false;
+      return false;
     }
+
+    if (!$scope.passwords.userpassword || !$scope.passwords.confirmpassword) {
+      $scope.passwordmismatch = false;
+      return false;
+    }
+
+    $scope.passwordmismatch =
+      $scope.passwords.userpassword !== $scope.passwords.confirmpassword;
+    return !$scope.passwordmismatch;
   };
 
   $scope.Apply = function(event) {
     $scope.formsubmitted = true;
-    if ($scope.checkPasswordsAreSame()) {
+    var formObj = $scope.changePassword;
+    var formIsValid = !(formObj && formObj.$invalid);
+
+    if ($scope.checkPasswordsAreSame() && formIsValid) {
       var url = URL + "cgi_action";
-      var data = "Newpassword=" + $scope.userpassword;
-      $http
-        .post(url, data)
+      // Change this line:
+      var payload =
+        "Newpassword=" + encodeURIComponent($scope.passwords.userpassword);
+
+      $http({
+        method: "POST",
+        url: url,
+        data: payload,
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      })
         .success(function(responseData, status) {
-          var formname = event.currentTarget.attributes["formname"].value;
+          var formname =
+            event &&
+            event.currentTarget &&
+            event.currentTarget.attributes &&
+            event.currentTarget.attributes["formname"]
+              ? event.currentTarget.attributes["formname"].value
+              : "changePassword";
           errorResponseDisplay(formname, responseData, status, event);
           console.log(status);
-          $location.path( '/quicksetup');
+          $location.path("/quicksetup");
         })
         .error(function(error) {});
     }
@@ -46,18 +70,21 @@ myapp.controller("changePasswordController", function(
 
   $scope.Skip = function() {
     $scope.formsubmitted = true;
-      var url = URL + "cgi_action";
-      var data = "Newpassword=V1120004";
+    var url = URL + "cgi_action";
+    var payload = "Newpassword=" + encodeURIComponent("V1120004");
 
-      $http
-        .post(url, data)
-        .success(function(responseData, status) {
-            $location.path( '/quicksetup');
-        })
-        .error(function(error) {
-          alert("Something Wrong happened, please try again");
-        });
-    
+    $http({
+      method: "POST",
+      url: url,
+      data: payload,
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+    })
+      .success(function(responseData, status) {
+        $location.path("/quicksetup");
+      })
+      .error(function(error) {
+        alert("Something Wrong happened, please try again");
+      });
   };
 
   function errorResponseDisplay(formname, response, status, event) {
@@ -67,12 +94,25 @@ myapp.controller("changePasswordController", function(
       $rootScope.$broadcast("rootScope:language_changed");
     } else if (500 <= status && status < 600) {
       $scope[formname + "popup"] = true;
-      $scope[formname + "popupval"] = data.Objects[0].Param[0].ParamValue;
+      $scope[formname + "popupval"] =
+        response &&
+        response.Objects &&
+        response.Objects[0] &&
+        response.Objects[0].Param &&
+        response.Objects[0].Param[0]
+          ? response.Objects[0].Param[0].ParamValue
+          : undefined;
     } else if (status == 207) {
       localStorage.setItem("multistatus", true);
       localStorage.setItem(
         "multistatusmessage",
-        data.Objects[0].Param[0].ParamValue
+        response &&
+          response.Objects &&
+          response.Objects[0] &&
+          response.Objects[0].Param &&
+          response.Objects[0].Param[0]
+          ? response.Objects[0].Param[0].ParamValue
+          : undefined
       );
       if (elementstatus != undefined) {
         $scope.Add(elementstatus);
@@ -80,7 +120,7 @@ myapp.controller("changePasswordController", function(
         $route.reload();
       }
     } else if (400 <= status && status < 500) {
-      angular.forEach(data.Objects, function(object) {
+      angular.forEach((response && response.Objects) || [], function(object) {
         var respobject = object.ObjName.replace(/\./g, "").replace(/\*/g, "");
         angular.forEach(object.Param, function(param) {
           $scope[respobject + "_" + param.ParamName + "responsestatus"] = true;
